5f1a5910d642fd5a02507072cfeffa59ea9ad6fe
Martins Innus <minnus@buffalo.edu>
2015-07-22 11:28:09 -0400

    pmlognow : Handle some cases of bad metrics

--- src/pmlognow/pmlognow.py
+++ src/pmlognow/pmlognow.py
@@ -19,6 +19,7 @@ import cpmapi as c_api
 from ctypes import c_char, c_int, c_uint, c_long, c_char_p, c_void_p
 
 import sys, math, datetime, getopt, time, socket
+import re
 
 import pprint
 
@@ -121,31 +122,31 @@ badidx = [idx for idx, pmid in enumerate(metric_ids) if pmid == c_api.PM_ID_NULL
 
 descs = []
 
-if len(badidx):
-    pmidGOOD = []
-    for i in range( len(nameA) ):
-        if i not in badidx:
-            # Also make sure we can get the desc before adding as a good metric
-            # Python API errors out as soon as one error is seen so can't check all at once
-            try:
-                desc = context.pmLookupDesc(metric_ids[i])
-                descs.append(desc)
-                pmidGOOD.append(metric_ids[i])
-            except pmapi.pmErr as error:
-                # Couldn't get the desc so just drop this metric
-                badidx.append(i)
-        else:
-            # Skip errors and dump this metric below
-            # We already handled nonleaf metrics above
-            pass
+# Deal with any entries in badidx already, and add new ones that pmLookupDesc fails to resolve
+pmidGOOD = []
+for i in range( len(nameA) ):
+    if i not in badidx:
+        # Also make sure we can get the desc before adding as a good metric
+        # Python API errors out as soon as one error is seen so can't check all at once
+        try:
+            desc = context.pmLookupDesc(metric_ids[i])
+            descs.append(desc)
+            pmidGOOD.append(metric_ids[i])
+        except pmapi.pmErr as error:
+            # Couldn't get the desc so just drop this metric
+            badidx.append(i)
+    else:
+        # Skip errors and dump this metric below
+        # We already handled nonleaf metrics above
+        pass
 
-    # Rebuild the list of metrics
-    # All should be good at this point
-    metric_ids = (c_uint * len(pmidGOOD))(*pmidGOOD)
+# Rebuild the list of metrics
+# All should be good at this point
+metric_ids = (c_uint * len(pmidGOOD))(*pmidGOOD)
 
-    # Remove bad ones from the list of metrics we know about
-    for idx in sorted(badidx, reverse=True):
-        del nameA[idx]
+# Remove bad ones from the list of metrics we know about
+for idx in sorted(badidx, reverse=True):
+    del nameA[idx]
 
 # Need to deal with errors with Fetch in some way
 results = context.pmFetch(metric_ids)
@@ -177,8 +178,17 @@ for i in range(results.contents.numpmid):
         else:
             raise
 
+    # Temporary hack for bad perfctr metric names
+    # Need to fix in the perfctr pmda
+    # The pmi API seems to be the only spot this is enforced
+    putname = re.sub(r'[-=]', '_', nameA[i])
+
+    # Skip Event Types for now
+    if( type == c_api.PM_TYPE_EVENT ):
+        continue
+
     # Add the Metric Definition
-    log.pmiAddMetric( nameA[i], metric_ids[i], type, descs[i].contents.indom, descs[i].contents.sem, descs[i].contents.units )
+    log.pmiAddMetric( putname, metric_ids[i], type, descs[i].contents.indom, descs[i].contents.sem, descs[i].contents.units )
 
     for j in range( results.contents.get_numval(i) ):
         atom = context.pmExtractValue(results.contents.get_valfmt(i),
@@ -194,9 +204,9 @@ for i in range(results.contents.numpmid):
             if inst_key not in inst_dict:
                 log.pmiAddInstance(descs[i].contents.indom, inames[j], insts[j])
                 inst_dict[inst_key] = 1
-            log.pmiPutValue( nameA[i], inames[j], "%s" % val )
+            log.pmiPutValue( putname, inames[j], "%s" % val )
         else:
-            log.pmiPutValue( nameA[i], "", "%s" % val )
+            log.pmiPutValue( putname, "", "%s" % val )
 
 timetuple = math.modf(time)
 useconds = int(timetuple[0] * 1000000)
